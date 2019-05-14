a7c84bd3ced86c45a488e7da7191059bc7dda04a
LaurenceM10 <laurenmontenegro10@gmail.com>
2019-05-13 23:50:37 -0600

    Test with prettier and colors from deno_std

---
+++ colors.ts
@@ -0,0 +1,3 @@
+import { bgWhite, green, bold } from "https://deno.land/std/colors/mod.ts";
+
+console.log(bgWhite(green(bold("Hello world!"))));
--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-import {serve} from "https://deno.land/std@v0.3.2/http/server.ts";
+import { serve } from "https://deno.land/std/http/server.ts";
 
 async function main() {
-    console.log("Server is running!");
-
-    const body = new TextEncoder().encode("Hello World\n");
-    for await (const req of serve(":8000")) {
-        req.respond({body});
-    }
+  console.log("Server is running!");
+  const body = new TextEncoder().encode("Hello World\n");
+  for await (const req of serve(":8000")) {
+    req.respond({ body });
+  }
 }
 
 main();
